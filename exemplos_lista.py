fd72a6ba37f81f68e4a32de224ae367bdd5953d2
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-07-09 09:18:52 -0300

    adicionando exemplos de lista

---
+++ exemplos_lista.py
@@ -0,0 +1,23 @@
+produto: str = "sapato"
+produto_2: str = "camiseta"
+produto_3: str = "videogame"
+
+produtos: list = []
+
+produtos.append(produto)
+print(produtos)
+produtos.append(produto_2)
+print(produtos)
+produtos.append(produto_3)
+print(produtos)
+
+#remover a última posição da lista
+print("Exemplo de pop, removendo a última posição da lista")
+produtos.pop()
+print(produtos)
+#remover o item desejado
+print("Exemplo de remove, removendo o que deseja retirar da lista")
+produtos.remove(produto)
+print(produtos)
+
+

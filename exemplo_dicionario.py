5f7430c7b4df205148410692041d704884e4d9f4
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-07-09 09:33:25 -0300

    adicionando exemplo de dicionario

---
+++ exemplo_dicionario.py
@@ -0,0 +1,28 @@
+ 
+import json
+
+
+dicionario: list = []
+
+produto_01 = {
+    "nome": "sapato",
+    "quantidade": 39,
+    "preco": 10.38,
+    "disponibilidade": True
+}
+
+produto_02 = {
+    "nome": "camiseta",
+    "quantidade": 15,
+    "preco": 15.01,
+    "disponibilidade": False
+}
+
+dicionario.append(produto_01)
+dicionario.append(produto_02)
+#print(dicionario)
+
+
+carrinho_compra = json.dumps(dicionario)
+print(carrinho_compra)
+

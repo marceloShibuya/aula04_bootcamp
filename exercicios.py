36b47d36fe0ce7e6db963b33896a03d8b66f326e
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-07-09 10:00:52 -0300

    adicionando exemplo de manipulacao de dicionario

--- exercicios.py
+++ exercicios.py
@@ -3,13 +3,13 @@
 
 from typing import Dict, Any
 
-dicionario = []
+# dicionario = []
 
-infos: Dict[str, Any] = {
-    "titulo": "Harry Potter",
-    "autor": "J.K.Rowling",
-    "ano_pub": 1995
-}
+# infos: Dict[str, Any] = {
+#     "titulo": "Harry Potter",
+#     "autor": "J.K.Rowling",
+#     "ano_pub": 1995
+# }
 
 # titulo = infos['titulo']
 # autor = infos['autor']
@@ -17,7 +17,26 @@ infos: Dict[str, Any] = {
 
 # print(f'O título do livro é {titulo}, autor {autor} e o ano de publicação foi em {ano}.')
 
-infos_elementos = infos.items()
+# infos_elementos = infos.items()
 
-for i in infos_elementos:
-    print(i)
+# for i in infos_elementos:
+#     print(i)
+
+
+#Exemplo criando um dicionário com dicionário dentro
+
+lista_de_livros_usando_dict: dict = {
+    "livro_01": {
+        "titulo": "Game of Thrones",
+        "autor": "estagiario",
+        "ano_pub": 2001
+    },
+    "livro_02": {
+        "titulo": "Harry Potter",
+        "autor": "J.K.Rowling",
+        "ano_pub": 1995
+    }  
+}
+
+#print(lista_de_livros_usando_dict["livro_02"])
+print(lista_de_livros_usando_dict["livro_02"]["titulo"])

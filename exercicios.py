fbea1dd9ccffc06b64df1ea292e5eac916902418
Marcelo Shibuya <marcelo.hshibuya@gmail.com>
2024-07-09 10:20:43 -0300

    adicionando exercicio 1

--- exercicios.py
+++ exercicios.py
@@ -1,7 +1,18 @@
-# Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. 
+
+# 1 - Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+
+lista: list = list(range(1,11))
+
+for i in lista:
+    print(i ** 2)
+
+# 2 - Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+
+# 3 - Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. 
 # Imprima cada informação.
 
-from typing import Dict, Any
+# from typing import Dict, Any
 
 # dicionario = []
 
@@ -25,18 +36,22 @@ from typing import Dict, Any
 
 #Exemplo criando um dicionário com dicionário dentro
 
-lista_de_livros_usando_dict: dict = {
-    "livro_01": {
-        "titulo": "Game of Thrones",
-        "autor": "estagiario",
-        "ano_pub": 2001
-    },
-    "livro_02": {
-        "titulo": "Harry Potter",
-        "autor": "J.K.Rowling",
-        "ano_pub": 1995
-    }  
-}
+# lista_de_livros_usando_dict: dict = {
+#     "livro_01": {
+#         "titulo": "Game of Thrones",
+#         "autor": "estagiario",
+#         "ano_pub": 2001
+#     },
+#     "livro_02": {
+#         "titulo": "Harry Potter",
+#         "autor": "J.K.Rowling",
+#         "ano_pub": 1995
+#     }  
+# }
 
-#print(lista_de_livros_usando_dict["livro_02"])
-print(lista_de_livros_usando_dict["livro_02"]["titulo"])
+# #print(lista_de_livros_usando_dict["livro_02"])
+# print(lista_de_livros_usando_dict["livro_02"]["titulo"])
+
+
+# 4- Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
+# 5 - Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
